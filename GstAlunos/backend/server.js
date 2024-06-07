43b2dcbe8923bf60f20f35981aaa1a6baaa506f9
riana023 <151479838+riana023@users.noreply.github.com>
2024-06-06 22:26:36 -0300

    atualização

--- GstAlunos/backend/server.js
+++ GstAlunos/backend/server.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const MONGODB_URI = process.env.MONGODB_URI || 'your_default_mongodb_uri_here';
+const MONGODB_URI = process.env.MONGO_URI; // Corrigido para MONGO_URI
 
 // Middleware
 app.use(cors());
@@ -17,14 +17,17 @@ app.use(express.json());
 // MongoDB conexão
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB conectado'))
-  .catch(err => console.error(err));
+  .catch(err => {
+    console.error('Erro ao conectar ao MongoDB:', err.message);
+    console.error('Stack trace:', err.stack);
+  });
 
 // Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/alunos', alunoRoutes);
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));  
+  app.use(express.static(path.join(__dirname, 'build')));
 }
 
 // Iniciar servidor
